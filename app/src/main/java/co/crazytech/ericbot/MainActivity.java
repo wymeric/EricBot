78038e94b0ad779e9e1ee011f9894c5e67fc4455
crazytechco <wymeric89@gmail.com>
2016-09-06 17:29:13 +0800

    new commit 201609060729

--- app/src/main/java/co/crazytech/ericbot/MainActivity.java
+++ app/src/main/java/co/crazytech/ericbot/MainActivity.java
@@ -7,6 +7,10 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,24 +19,36 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 import co.crazytech.ericbot.bt.BtDeviceListActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
     //activity result identifier
     private static final int DEVICERES = 0;
+
+    private MotorAction motor;
+    private boolean goPressed,reversePressed;
 
     //bluetooth
     private BluetoothSocket btSocket;
 
+    //Accelerometer
+    private SensorManager sensorMan;
+    private Sensor sensorGyro;
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    private static final float EPSILON = 0.00001f;
+    private float timestamp;
+
     //widgets
     Button btnGo,btnStop,btnDevices,btnDetach,btnResetServo;
     SeekBar seekBar;
+    TextView tvGyros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +56,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        motor = new MotorAction();
+
         //call widgets
+        tvGyros = (TextView)findViewById(R.id.textViewGyros);
         btnDevices = (Button)findViewById(R.id.buttonDevices);
         btnDevices.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,18 +87,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        if(btSocket!=null&&btSocket.isConnected()) try {
-                            btSocket.getOutputStream().write(String.valueOf("1:"+1).getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        motor.forward();
+                        goPressed = true;
                         return true; // if you want to handle the touch event
                     case MotionEvent.ACTION_UP:
-                        if(btSocket!=null&&btSocket.isConnected()) try {
-                            btSocket.getOutputStream().write(String.valueOf("1:"+3).getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        motor.stop();
+                        goPressed = false;
                         return true; // if you want to handle the touch event
                 }
                 return false;
@@ -91,18 +104,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 switch(event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        if(btSocket!=null&&btSocket.isConnected()) try {
-                            btSocket.getOutputStream().write(String.valueOf("1:"+2).getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        motor.reverse();
+                        reversePressed = true;
                         return true; // if you want to handle the touch event
                     case MotionEvent.ACTION_UP:
-                        if(btSocket!=null&&btSocket.isConnected()) try {
-                            btSocket.getOutputStream().write(String.valueOf("1:"+3).getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        motor.stop();
+                        reversePressed = false;
                         return true; // if you want to handle the touch event
                 }
                 return false;
@@ -144,13 +151,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //call sensor
+        sensorMan = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensorGyro = sensorMan.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        sensorMan.registerListener(this, sensorGyro,SensorManager.SENSOR_DELAY_NORMAL);
+        float inR[] = new float[9];
+        float outR[] = new float[9];
+        sensorMan.remapCoordinateSystem(inR,sensorMan.AXIS_Y,sensorMan.AXIS_MINUS_X,outR);
     }
 
     @Override
     public void finish() {
         if(btSocket!=null&&btSocket.isConnected())btnDetach.callOnClick();
+        sensorMan.unregisterListener(this);
         super.finish();
     }
+
+    @Override
+    protected void onPause() {
+        sensorMan.unregisterListener(this);
+        if(btSocket!=null)try {
+            btSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorMan.registerListener(this, sensorGyro,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -164,6 +198,67 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Float rollVal = event.values[1];
+        tvGyros.setText("ROLL :"+ Float.toString(rollVal));
+        if(rollVal>15f||rollVal<-15f) {
+            if (rollVal > 15f && goPressed) {
+                motor.turnLeft();
+            } else if (rollVal < -15f && goPressed) {
+                motor.turnRight();
+            } else if (goPressed) motor.forward();
+            else if (reversePressed) motor.reverse();
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private class MotorAction {
+        public void forward(){
+            if(btSocket!=null&&btSocket.isConnected()) try {
+                btSocket.getOutputStream().write(String.valueOf("1:"+1).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void reverse(){
+            if(btSocket!=null&&btSocket.isConnected()) try {
+                btSocket.getOutputStream().write(String.valueOf("1:"+2).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void stop(){
+            if(btSocket!=null&&btSocket.isConnected()) try {
+                btSocket.getOutputStream().write(String.valueOf("1:"+3).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void turnLeft(){
+            if(btSocket!=null&&btSocket.isConnected()) try {
+                btSocket.getOutputStream().write(String.valueOf("1:"+4).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void turnRight(){
+            if(btSocket!=null&&btSocket.isConnected()) try {
+                btSocket.getOutputStream().write(String.valueOf("1:"+5).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 
     private class ConnectBT extends AsyncTask<String,Void,Void> {
         private boolean connectSuccess = true;
